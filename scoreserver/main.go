f678bf126d3f6fdf44e37df768ca45c4fb4357f3
Masanori Tani <metro@uta8a.org>
2022-11-23 01:44:03 +0000

    fix(scoreserver): jsonを入力に入れて、logを構造化した

--- scoreserver/main.go
+++ scoreserver/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 	"github.com/srinathgs/mysqlstore"
 	"github.com/ziflex/lecho/v3"
 )
@@ -25,13 +26,25 @@ type HealthCheckResponse struct {
 	Detail  string `json:"detail,omitempty"`
 }
 
+type Player struct {
+	Username string `json:"username"`
+	Score    string `json:"score"`
+}
+
+type PlayersResponse struct {
+	Players []Player `json:"players"`
+}
+
 func main() {
 	e := echo.New()
 	e.Logger = lecho.New(os.Stdout)
 
 	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
-		e.Logger.Fatalf("[get TimeZone info] %v", err)
+		e.Logger.Fatalj(log.JSON{
+			"message": "get TimeZone info",
+			"detail":  err.Error(),
+		})
 	}
 
 	config = mysql.Config{
@@ -47,13 +60,19 @@ func main() {
 	}
 	_db, err := sqlx.Connect("mysql", config.FormatDSN())
 	if err != nil {
-		e.Logger.Fatalf("[get mysql connection] %v", err)
+		e.Logger.Fatalj(log.JSON{
+			"message": "get mysql connection",
+			"detail":  err.Error(),
+		})
 	}
 	db = _db
 
 	_store, err := mysqlstore.NewMySQLStoreFromConnection(db.DB, "sessions", "/", 60*60*24*14, []byte("secret-token"))
 	if err != nil {
-		e.Logger.Fatalf("[create session store] %v", err)
+		e.Logger.Fatalj(log.JSON{
+			"message": "create session store",
+			"detail":  err.Error(),
+		})
 	}
 	store = _store
 
@@ -62,6 +81,7 @@ func main() {
 	// Path
 	e.GET("/.healthcheck", getHealthCheck)
 	e.GET("/.healthcheck/deep", getDeepHealthCheck)
+	// e.GET("/players", getPlayers)
 	e.Start(":3000")
 }
 
@@ -74,7 +94,10 @@ func getHealthCheck(c echo.Context) error {
 
 func getDeepHealthCheck(c echo.Context) error {
 	if err := db.Ping(); err != nil {
-		c.Echo().Logger.Errorf("[get deep healthcheck] %v", err)
+		c.Echo().Logger.Errorj(log.JSON{
+			"message": "get deep healthcheck",
+			"detail":  err.Error(),
+		})
 		response := HealthCheckResponse{
 			Message: "failed to get connection to db",
 			Detail:  err.Error(),
@@ -86,3 +109,17 @@ func getDeepHealthCheck(c echo.Context) error {
 	}
 	return c.JSON(http.StatusOK, response)
 }
+
+// func getPlayers(c echo.Context) error {
+//   var players []Player
+//   players = make([]Player, 0)
+//   rows, err := db.Query("SELECT username, score FROM users WHERE role = 'player'")
+//   if err != nil {
+//     c.Echo().Logger.Errorf("")
+//     return c.JSON()
+//   }
+//   response := PlayersResponse {
+//     Players: ,
+//   }
+//   return c.JSON(http.StatusOK, response)
+// }
