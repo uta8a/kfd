389599aa1fe447a9f796a93baf37a8dd4525c6f4
Masanori Tani <metro@uta8a.org>
2022-11-29 12:48:23 +0000

    fix(misc): change example data

--- misc/db/initdb.d/2_data.sql
+++ misc/db/initdb.d/2_data.sql
@@ -1,15 +1,15 @@
-INSERT INTO users (username, password_hash, role) VALUES ('Alice', 'passhash1', 'admin');
-INSERT INTO users (username, password_hash, score, role) VALUES ('Bob', 'passhash2', 0, 'player');
-INSERT INTO users (username, password_hash, role) VALUES ('Conishi', 'passhash3', 'admin');
-INSERT INTO users (username, password_hash, score, role) VALUES ('Dvorak', 'passhash4', 0, 'player');
-INSERT INTO users (username, password_hash, score, role) VALUES ('Eimy', 'passhash5', 0, 'player');
+INSERT INTO users (`username`, `password_hash`, `role`) VALUES ('Alice', 'passhash1', 'admin');
+INSERT INTO users (`username`, `password_hash`, `score`, `role`) VALUES ('Bob', 'passhash2', 0, 'player');
+INSERT INTO users (`username`, `password_hash`, `role`) VALUES ('Conishi', 'passhash3', 'admin');
+INSERT INTO users (`username`, `password_hash`, `score`, `role`) VALUES ('Dvorak', 'passhash4', 0, 'player');
+INSERT INTO users (`username`, `password_hash`, `score`, `role`) VALUES ('Eimy', 'passhash5', 0, 'player');
 
-INSERT INTO challenges (title, description, flag, points) VALUES ('chall 1', 'Easy Challenge!', 'CTF{this_is_flag_1}', 100);
-INSERT INTO challenges (title, description, flag, points) VALUES ('chall 2', 'Med Challenge!', 'CTF{this_is_flag_2}', 100);
-INSERT INTO challenges (title, description, flag, points) VALUES ('chall 3', 'Hard Challenge!', 'CTF{this_is_flag_3}', 100);
-INSERT INTO challenges (title, description, flag, points) VALUES ('chall 4', 'Super Challenge!', 'CTF{this_is_flag_4}', 100);
-INSERT INTO challenges (title, description, flag, points) VALUES ('chall 5', 'Hyper Challenge!', 'CTF{this_is_flag_5}', 100);
+INSERT INTO challenges (`slug`, `title`, `description`, `flag`, `points`) VALUES ('easy', 'chall 1', 'Easy Challenge!', 'CTF{this_is_flag_1}', 100);
+INSERT INTO challenges (`slug`, `title`, `description`, `flag`, `points`) VALUES ('med', 'chall 2', 'Med Challenge!', 'CTF{this_is_flag_2}', 100);
+INSERT INTO challenges (`slug`, `title`, `description`, `flag`, `points`) VALUES ('hard', 'chall 3', 'Hard Challenge!', 'CTF{this_is_flag_3}', 100);
+INSERT INTO challenges (`slug`, `title`, `description`, `flag`, `points`) VALUES ('super', 'chall 4', 'Super Challenge!', 'CTF{this_is_flag_4}', 100);
+INSERT INTO challenges (`slug`, `title`, `description`, `flag`, `points`) VALUES ('hyper', 'chall 5', 'Hyper Challenge!', 'CTF{this_is_flag_5}', 100);
 
-INSERT INTO submissions (user_id, challenge_id, correctness) VALUES (1,1,false);
+INSERT INTO submissions (`user_id`, `challenge_id`, `correctness`) VALUES (1,1,false);
 
-INSERT INTO attachments (name, url, challenge_id) VALUES ('chall.zip','https://example.com/chall.zip',1);
+INSERT INTO attachments (`name`, `key`, `challenge_id`) VALUES ('chall.zip','chall.zip', 1);
